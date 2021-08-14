37854295e7569ef5ef9139a89f190ef2ee6166f4
Ronald Seah <ronseahald@hotmail.com>
2021-08-14 22:50:36 +0800

    Add missing slash

--- voting-frontend/src/routing/VotingFrontendSwitch.tsx
+++ voting-frontend/src/routing/VotingFrontendSwitch.tsx
@@ -7,7 +7,7 @@ import { EnrollVoterForm } from 'components/vote-admin/EnrollVoterForm'
 export const VotingFrontendSwitch: React.FC = () => {
     return (
         <Switch>
-            <Route path="enroll" exact>
+            <Route path="/enroll" exact>
                 <EnrollVoterForm />
             </Route>
         </Switch>
