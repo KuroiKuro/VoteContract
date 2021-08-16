ee075d523e1850b64eda88c9423ad171768ea7d9
Ronald Seah <ronseahald@hotmail.com>
2021-08-16 22:00:50 +0800

    Use proper methods to call contract method

--- voting-frontend/src/lib/ethers-lib.ts
+++ voting-frontend/src/lib/ethers-lib.ts
@@ -12,11 +12,19 @@ declare global {
 export const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 
+// Utility functions to easily retrieve objects and classes needed to call
+// smart contract methods
+
+export const getProvider = (): ethers.providers.Web3Provider => {
+    if (!window.ethereum) throw new Error("Metamask not installed");
+    return new ethers.providers.Web3Provider(window.ethereum);
+}
+
 export const getContractObject = (): ethers.Contract => {
     // Utility function to create a Contract object
-    if (!window.ethereum) throw new Error("Metamask not installed");
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    return new ethers.Contract(CONTRACT_ADDRESS, VoteContract.abi, provider);
+    const provider = getProvider();
+    const signer = provider.getSigner();
+    return new ethers.Contract(CONTRACT_ADDRESS, VoteContract.abi, signer);
 }
 
 
@@ -24,7 +32,8 @@ export const enrollVoters = async (voterAddresses: string[]) => {
     // Call the smart contract method to enroll voters
     const contract = getContractObject();
     try {
-        await contract.enrollVoters(voterAddresses);
+        const transaction = await contract.enrollVoters(voterAddresses);
+        await transaction.wait();
     } catch (err) {
         console.log(`Error when enrolling voters: ${err}`);
     }
