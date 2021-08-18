7e8369984547438d87a8935df8391ad73d760a53
Ronald Seah <ronseahald@hotmail.com>
2021-08-18 21:17:35 +0800

    Enroll sample voter when deploying contract

--- VoteContract/scripts/deploy-votecontract.ts
+++ VoteContract/scripts/deploy-votecontract.ts
@@ -14,7 +14,11 @@ const main = async () => {
     // the frontend can access using process.env
     const contractAddress = VoteContract.address;
     console.log("VoteContract deployed to:", contractAddress);
-    fs.writeFileSync("./contract-address.txt", contractAddress);
+    // Automatically enroll account 19 into voter roll
+    console.log("Enrolling sample voter address");
+    const autoVoterAddress = "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199";
+    await VoteContract.enrollVoters([autoVoterAddress]);
+    console.log("Enrolled voter address");
 }
 
 
